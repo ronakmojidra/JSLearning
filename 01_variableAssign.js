b4e52a4cb893d40d48971ee0bbcfd6b133869ef3
ronakmojidra <mojidraronak@gmail.com>
2023-07-19 18:54:46 +0530

    Variable assignment updated
    Variable assignment updated

---
+++ 01_variableAssign.js
@@ -0,0 +1,32 @@
+var myFullName;
+console.log(myFullName);
+
+myFullName = "Ronak Mojidra";
+console.log(myFullName);
+
+myFullName = "Ronak Kishor Mojidra";
+console.log(myFullName);
+
+var myLove = "I love only JS";
+console.log(myLove);
+
+var firstName = "Ronak";
+console.log(firstName);
+
+var lastName = "Mojidra";
+console.log(lastName);
+
+var age = 31;
+console.log(age);
+
+var address = "258 Shukrawar Peth, Panchavati Heights, 3rd Floor, D-6, Pune";
+console.log(address);
+
+var pinCode = 411002;
+console.log(pinCode);
+
+var gender = "Male";
+console.log(gender);
+
+var isMarried = false;
+console.log(isMarried);
