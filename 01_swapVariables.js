25a54151258016f991c08b137117fef5e671c04f
ronakmojidra <mojidraronak@gmail.com>
2023-07-20 12:03:25 +0530

    Updated
    Updated

---
+++ 01_swapVariables.js
@@ -0,0 +1,21 @@
+console.log("==========1.Swap two variables==========");
+var sweety = "Sweety";
+var cutie = "Cutie";
+console.log('Before swap ===>','sweety:', sweety,',' ,'cutie:', cutie);
+
+temp = sweety;
+sweety = cutie;
+cutie = temp;
+console.log('after swap ===>','sweety:', sweety,',' ,'cutie:', cutie);
+
+console.log("==========2.Swap three variables==========");
+var num1 = 100;
+var num2 = 200;
+var num3 = 300;
+console.log('Before swap ===>','num1:', num1, ',', 'num2:', num2, ',', 'num3:',num3);
+temp = num1;
+num1 = num2;
+num2 = num3;
+num3 = temp;
+console.log('After swap ===>','num1:', num1, ',', 'num2:', num2, ',', 'num3:',num3);
+
