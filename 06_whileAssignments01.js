1a8c5173d1851b4aee24e349aca26608e7532106
ronakmojidra <mojidraronak@gmail.com>
2023-07-31 12:27:12 +0530

    For and While loop Assignment completed
    For and While loop Assignment completed

---
+++ 06_whileAssignments01.js
@@ -0,0 +1,48 @@
+console.log(`1>.WAP to print numbers from 5 to 15 by incrementing 1`);
+var i = 5;
+while (i <= 15) {
+    console.log(`${i}`);
+    i++;
+}
+
+console.log(`2>.WAP to print numbers from 50 to 40 by decrementing 1`);
+var i = 50;
+while (i >= 40) {
+    console.log(`${i}`);
+    i--;
+}
+
+console.log(`3>.WAP to find first 15 odd numbers`);
+var i = 1;
+while (i <= 30) {
+    console.log(`${i}`);
+    i = i + 2; 
+}
+
+console.log(`4>.WAP to find first 10 even numbers`);
+var i = 0;
+while (i < 20) {
+    console.log(`${i}`);
+    i = i + 2;
+}
+
+console.log(`5>.WAP to print table of 5`);
+var i = 5;
+while (i <= 50) {
+    console.log(`${i}`);
+     i = i + 5;
+}
+
+console.log(`6>.WAP to print table of 10`);
+var i = 10;
+while (i <= 100) {
+    console.log(`${i}`);
+    i = i + 10;
+}
+
+console.log(`7>.WAP to print table of 10 in reverse order`);
+var i = 100;
+while (i >= 10) {
+    console.log(`${i}`);
+    i = i - 10;
+}
